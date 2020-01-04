8b4ec62abdc1fae6b355c1278240e1cae5d2da60
1106108136 <1106108136@nkust.edu.tw>
2020-01-04 15:13:23 +0800

    feat: 新增選單
    新增選擇學生用的選單
    
    issue #10

--- application/modules/elective/controllers/Elective.php
+++ application/modules/elective/controllers/Elective.php
@@ -2,15 +2,25 @@
 
 /**
  * Class Elective
+ *
+ * @property Student module $student
+ * @property Student_model $student_model
  */
 class Elective extends MX_Controller
 {
+    /**
+     * @var Student module
+     */
+    private $student;
+
     /**
      * Elective constructor.
      */
     public function __construct()
     {
         parent::__construct();
+        $this->student = Modules::load('student');
+        $this->student->load->model('Student_model', 'student_model');
     }
 
     /**
@@ -18,7 +28,8 @@ class Elective extends MX_Controller
      */
     public function index()
     {
-        $this->load->view('elective');
+        $result = $this->student_model->get_data();
+        $this->load->view('elective', array('students' => $result->result_array()));
     }
 }
 
--- application/modules/elective/views/elective.php
+++ application/modules/elective/views/elective.php
@@ -3,13 +3,27 @@
 <head>
     <meta charset="utf-8">
     <title>選課作業系統</title>
-    <script type="text/javascript" src="<?= base_url() ?>assets/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="<?= base_url() ?>assets/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="<?= base_url() ?>assets/js/elective.js"></script>
+    <script type="text/javascript">
+        let base_url = function () {
+            return "<?= base_url() ?>";
+        };
+    </script>
 </head>
 <body>
 
 <div id="container" align="center">
     <h1>選課作業系統</h1>
     <button onclick="window.location.assign('<?= base_url() ?>')">首頁</button>
+    <br><br>
+    <form>
+        <select name="student">
+            <? foreach ($students as $student): ?>
+                <option value="<?=$student['學號']?>"><?=$student['學號'] . ' ' . $student['姓名']?></option>
+            <? endforeach; ?>
+        </select>
+    </form>
 </div>
 
 </body>
